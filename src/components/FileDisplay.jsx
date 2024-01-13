c51a27b5b25f8c714f4d71ea776b85584a7c97cc
cmcelduff <111883363+cmcelduff@users.noreply.github.com>
2024-01-13 22:06:46 +0000

    props changes

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function FileDisplay(props) {
-        const {handleAudioReset, file, audioStream} = props;
+        const {handleAudioReset, file, audioStream, handleFormSubmission} = props;
 
         return (
             <main className='flex-1 p-4 flex-col gap-3 text-center sm:gap-4 justify-center pb-20 w-72 sm:96 max-w-full mx-auto'>
@@ -12,7 +12,7 @@ export default function FileDisplay(props) {
                 </div>
                 <div className='flex items-center justify-between gap-4'>
                     <button onClick={handleAudioReset} className='text-slate-400 hover:text-blue-600'>Reset</button>
-                    <button className='specialBtn px-3 py-2 rounded-lg text-blue-400 flex items-center gap-2 font-medium'><p>Transcribe</p></button>
+                    <button onClick={handleFormSubmission} className='specialBtn px-3 py-2 rounded-lg text-blue-400 flex items-center gap-2 font-medium'><p>Transcribe</p></button>
                     <i className="fa-solid fa-pen-nib"></i>
                 </div>
                 </main>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -82,7 +82,7 @@ export default function HomePage(props) {
                 const tempFile = e.target.files[0]
                 setFile(tempFile)
             }} className='hidden' type='file' accept='.mp3,.wave' /></label> a mp3 file</p>
-            <p className='italic text-slate-400'>Free now free forever</p>
+            <p className='italic text-slate-400'>Scribe anytime!</p>
         </main>
     )
 }
